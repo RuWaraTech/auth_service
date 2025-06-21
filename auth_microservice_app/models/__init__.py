0fa2dd7a1b51df4625236520812bec80d99986c3
Farai Wande <farai.wande@asda.co.uk>
2025-06-21 01:58:11 +0100

    Reorganize import statements and update __all__ to include only User

--- auth_microservice_app/models/__init__.py
+++ auth_microservice_app/models/__init__.py
@@ -1,10 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from auth_microservice_app.models.user import User
-
 db = SQLAlchemy()
 
+from auth_microservice_app.models.user import User
 
 __all__ = [
-	'db',
-	'User',
+    
+    "User"
 ]
