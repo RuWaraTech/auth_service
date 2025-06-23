e700f92e6be55b5fcf005bffa4275426fa79d3e6
Farai Wande <farai.wande@asda.co.uk>
2025-06-23 17:25:18 +0100

    refactor: include UserSession in __all__ for model exports

--- auth_microservice_app/models/__init__.py
+++ auth_microservice_app/models/__init__.py
@@ -5,9 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 from auth_microservice_app.models.user import User
+from auth_microservice_app.models.user_session import UserSession
 
 __all__ = [
     "db",
-    "User"
+    "User",
+    "UserSession"
 ]
 
