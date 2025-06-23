3ba18beb53e23cae0b0462d60b7a703eb6f80fd9
Farai Wande <farai.wande@asda.co.uk>
2025-06-23 18:23:11 +0100

    refactor: remove commented-out code for blueprint registration in routes

--- auth_microservice_app/routes/__init__.py
+++ auth_microservice_app/routes/__init__.py
@@ -8,14 +8,6 @@ from auth_microservice_app.routes.core_auth import core_auth_bp
 __all__ = ['health_bp', 'auth_bp','core_auth_bp']
 
 
-# # Function to register all blueprints
-# def register_all_blueprints(app):
-#     """Register all application blueprints with the Flask app."""
-  
-    
-#     api_prefix = f"/api/{app.config.get('API_VERSION', 'v1')}"
-#     app.register_blueprint(auth_bp, url_prefix=f"{api_prefix}/auth")
-#     app.register_blueprint(health_bp)
     
     
     
