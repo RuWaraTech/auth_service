c3f0b33c5de71b042989f84d03619403904fbd37
Farai Wande <farai.wande@asda.co.uk>
2025-06-23 17:13:33 +0100

    refactor: update docstring format and ensure newline at end of file in models init

--- auth_microservice_app/models/__init__.py
+++ auth_microservice_app/models/__init__.py
@@ -1,9 +1,13 @@
+"""
+Database models for the authentication microservice.
+"""
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 from auth_microservice_app.models.user import User
 
 __all__ = [
-    
+    "db",
     "User"
 ]
+
